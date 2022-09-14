1a1fe769e1e8d9c5e5fcbb19d45674fb592e945d
CIT142F-7@M307.UC-BCF.EDU.PH <CIT142F-7@M307.UC-BCF.EDU.PH>
2022-09-14 16:43:11 +0800

    -New Push

---
+++ TokenPush.bat
@@ -0,0 +1,11 @@
+@echo off 
+
+git remote set-url origin https://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@github.com/clmjuls/CIT142F.git
+
+git add .
+
+git commit -m -"New Push"
+
+git push
+
+pause
--- push.bat
+++ push.bat
@@ -1,7 +1,5 @@
 @echo off 
 
-git remote set-url origin https://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@github.com/clmjuls/CIT142F.git
-
 git add .
 
 git commit -m -"New Push"
